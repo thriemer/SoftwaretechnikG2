e9d57d49b4138b643b0ce318f1669e1ccb0b8abc
linus <linus.thriemer@gmx.de>
2022-12-03 14:52:11 +0100

    add controller

---
+++ coder/backend/src/main/java/de/softwaretechnik/coder/adapter/primary/TaskController.java
@@ -0,0 +1,26 @@
+package de.softwaretechnik.coder.adapter.primary;
+
+import de.softwaretechnik.coder.application.SolutionService;
+import de.softwaretechnik.coder.domain.Task;
+import de.softwaretechnik.coder.domain.TestResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class TaskController {
+
+    private final SolutionService solutionService;
+
+    @PostMapping("/api/task/submit")
+    ResponseEntity<TestResult[]> submitSolution(@RequestParam String taskName, @RequestBody String solution) {
+        return ResponseEntity.ok(solutionService.testResults(solution, taskName));
+    }
+
+    @GetMapping("/api/task/listAll")
+    ResponseEntity<Task[]> getTaskByName() {
+        return ResponseEntity.ok(solutionService.getAllTasks());
+    }
+
+}
