6455e099a08869a0b3780a258f7cd605eee21ccb
RollLeon <117123568+RollLeon@users.noreply.github.com>
2023-01-18 16:15:53 +0100

    Update CodeTask.java

--- coder/backend/src/main/java/de/softwaretechnik/coder/domain/CodeTask.java
+++ coder/backend/src/main/java/de/softwaretechnik/coder/domain/CodeTask.java
@@ -1,7 +1,10 @@
 package de.softwaretechnik.coder.domain;
 
 public record CodeTask(
-        String name,
+        String name, //name in database
+        String displayName, //name in frontend
+        String shortDescription, //description in frontend
+        String taskType,
         String taskDescription,
         String codeTemplate
 ) {
