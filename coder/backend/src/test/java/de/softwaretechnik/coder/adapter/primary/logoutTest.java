b66df004651e9c9006081f6f7fe0feb129671746
dfl22 <118337617+dfl22@users.noreply.github.com>
2023-01-27 16:08:54 +0100

    basic setup

---
+++ coder/backend/src/test/java/de/softwaretechnik/coder/adapter/primary/logoutTest.java
@@ -0,0 +1,26 @@
+package de.softwaretechnik.coder.adapter.primary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class logoutTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void logout_success_redirectedtosuccpage() throws Exception {
+        mockMvc.perform(get("/logout"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrlPattern("**/succ"));
+    }
+
+}
