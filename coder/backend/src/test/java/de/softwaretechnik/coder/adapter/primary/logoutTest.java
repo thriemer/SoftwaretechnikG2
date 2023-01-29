05a37784e97621b5c48c35c5bddab42a997e0d84
linus <linus.thriemer@gmx.de>
2023-01-29 21:21:40 +0100

    fixed test

--- coder/backend/src/test/java/de/softwaretechnik/coder/adapter/primary/logoutTest.java
+++ coder/backend/src/test/java/de/softwaretechnik/coder/adapter/primary/logoutTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
@@ -20,7 +21,7 @@ class logoutTest {
     void logout_success_redirectedtosuccpage() throws Exception {
         mockMvc.perform(get("/logout"))
                 .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrlPattern("**/succ"));
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/succ"));
     }
 
 }
